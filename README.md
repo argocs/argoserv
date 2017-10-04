b93fd70e9809a079585c194ea211257e47a937f6
dashaw92 <dashaw92@gmail.com>
2017-10-03 21:45:09 -0400

    Rewrote server from scratch Server now serves files Serves index.gph files as gophermenu Included example site to play with

--- README.md
+++ README.md
@@ -1,13 +1,36 @@
 ### argoserv  
 A Gopher server implemented in Rust
 
+### Making a server
+
+This server only serves `index.gph` files in directories, so you must include one for every directory you wish to have in the server.
+
+`index.gph`'s are plain text documents that conform to the Gopher protocol.
+
+The server will replace the following tokens automatically:
+
+`$ADDRESS$` -> The IP address specified at runtime.  
+`\t` -> A tab character. Note this is a backslash followed by a `t`. Not an actual tab character.
+
+I recommend using `$ADDRESS$` for local host selectors in your `index.gph`s so you need not rewrite the IP everytime it changes.
+
+See included example site for working examples `index.gph`s.
+
 ### Usage
 
 ```
-$ ./argoserv <ip>
+$ ./argoserv <ip> [site directory]
 argoserv listening on <ip>:70
+```
+
+### Example gopher site
+
+This repo comes with an example site that was used while testing. You may run it like so:
+
+```
+$ cargo run -- <ip> site
 ```
 
 ### License
 
-TODO
+MIT. See `LICENSE`
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,56 @@
 #![allow(unused_must_use)]
 use std::net::{TcpStream, TcpListener};
-use std::path::Path;
-use std::fs::{self, ReadDir, File};
+use std::path::{Path, PathBuf};
+use std::fs::File;
 use std::io::{Read, Write, BufReader};
 use std::thread;
 use std::env;
-
-/*
- * TODO
- *  > Serving gophermap by default (index.gph)
- *  > File sending
- *  > Custom directory as an argument
- *  > Gophermap
- */
 
 fn main() {
     let ipaddr = match env::args().nth(1) {
         Some(ipaddr) => ipaddr,
         None => {
-            eprintln!(
-                "Usage: {} <address to bind to>",
-                env::args().nth(0).unwrap_or("./argoserv".to_owned())
-            );
+            eprintln!("Missing IP address to bind to.");
             return;
         }
     };
-    let server = TcpListener::bind(&format!("{}:70", ipaddr)).unwrap();
-    println!(
-        "argoserv listening on {}:{}",
-        server.local_addr().unwrap().ip(),
-        server.local_addr().unwrap().port()
-    );
+    let directory = match env::args().nth(2) {
+        Some(dir) => dir,
+        None => ".".to_owned(),
+    };
+    let address = &format!("{}:70", ipaddr);
+    let server = match TcpListener::bind(address) {
+        Ok(server) => server,
+        Err(_) => {
+            eprintln!("Failed to start server.");
+            return;
+        }
+    };
+    println!("argoserv listening on {}:70", ipaddr);
+
     for client in server.incoming() {
         match client {
             Ok(client) => {
-                //println!("CONNECT <- {}", client.peer_addr().unwrap().ip()); // This is pretty
-                //spammy
-                thread::spawn(move || { handle(client); });
+                let addr = ipaddr.clone();
+                let dir = directory.clone();
+                thread::spawn(move || { handle(client, addr, dir); });
             }
             Err(e) => eprintln!("ERROR : {:?}", e),
         }
     }
 }
 
-fn handle(mut client: TcpStream) {
-    let mut buffer = [0; 512];
+fn handle(mut client: TcpStream, ipaddr: String, directory: String) {
+    let client_ip = client.peer_addr().unwrap().ip();
+
+    let mut buffer = [0; 1024];
     let read = client.read(&mut buffer);
-    match read {
+    let mut selector = match read {
         Ok(n) => {
-            //The client has sent the request. buffer = [ selector... \r\n ]
-            let mut selector = String::new();
+            let mut wordbuf = String::new();
             let mut index = 0;
             'outer: while index < n {
-                selector.push(buffer[index] as char);
+                wordbuf.push(buffer[index] as char);
                 if index + 2 < buffer.len() {
                     //If the next two bytes are \r\n, we're done parsing the selector.
                     if buffer[index + 1] == 0x0d && buffer[index + 2] == 0x0a {
@@ -61,84 +59,82 @@ fn handle(mut client: TcpStream) {
                 }
                 index += 1;
             }
-            println!(
-                "LIST '{}' -> {}",
-                selector,
-                client.peer_addr().unwrap().ip()
-            );
-            // Prepend a "." to the selector to keep us in the working directory of the server
-            if selector == "/" {
-                selector = ".".to_owned();
-            } else if selector.starts_with("/") {
-                selector = ".".to_owned() + &selector;
-            }
-            let borrowedsel = selector.clone();
-            let cwd = env::current_dir().unwrap();
-            let mut path = Path::new(&borrowedsel);
-            // Check if the path the client requested is outside of our jail
-            if !path.exists() ||
-                !path.canonicalize().unwrap().starts_with(
-                    cwd.as_path()
-                        .canonicalize()
-                        .unwrap()
-                        .as_path(),
-                )
-            {
-                //Reset them to the root of our jail
-                path = cwd.as_path();
-                selector = ".".to_owned();
-            }
-            if path.is_file() {
-                if path.extension().unwrap() == "gph" {
-                    //This is a gopher menu, serve it to the client
-                    let contents = read_file(&path);
-                    match contents {
-                        Ok(contents) => {
-                            for line in contents.lines() {
-                                let mut line = line.to_owned();
-                                line += "\r\n";
-                                client.write(line.as_bytes());
-                            }
-                        }
-                        Err(_) => {
-                            client.write(b"iFailed to open requested file.\r\n.");
-                        }
-                    }
-                }
-            } else {
-                client.write(b"iargoserv v1 - October 2017 - dashaw92\r\n");
-                let iter: ReadDir = fs::read_dir(&path).unwrap();
-                for entry in iter {
-                    if let Ok(entry) = entry {
-                        let ltype = if entry.path().is_file() { "0" } else { "1" };
-                        let selector =
-                            format!("{}/{}", selector, entry.file_name().to_str().unwrap());
-                        client.write(
-                            format(
-                                ltype,
-                                entry.file_name().to_str().unwrap(),
-                                selector.as_str(),
-                                "192.168.1.115",
-                                70,
-                            ).as_bytes(),
-                        );
-                    }
-                }
+            wordbuf
+        }
+        Err(e) => {
+            eprintln!("READ ERROR {} <- {}", e, client_ip);
+            return;
+        }
+    };
+    println!("LIST '{}' -> {}", selector, client_ip);
+    // Prepend a "." to the selector to keep us in the working directory of the server
+    if selector == "/" {
+        selector = ".".to_owned();
+    } else if selector.starts_with("/") {
+        selector = ".".to_owned() + &selector;
+    }
+
+    let _selector = selector.clone();
+    let home = Path::new(&directory); //The home directory of the server
+    let mut dest = Path::new(&directory).to_path_buf();
+    dest.push(_selector);
+    let mut dest = dest.as_path();
+
+    let canonical_path = dest.canonicalize().unwrap_or(PathBuf::new());
+    let canonical_cwd = home.canonicalize().unwrap_or(PathBuf::new());
+    if !dest.exists() {
+        client.write(b"iNo content.\r\n");
+        client.write(&format!("1Return home\t/\t{}\t70\r\n.", ipaddr).as_bytes());
+        return;
+    } else if !canonical_path.starts_with(canonical_cwd) {
+        dest = Path::new(&directory);
+    }
+
+    //Serve the client
+    if dest.is_file() {
+        //Send the data of this file.
+        let mut file = File::open(dest).unwrap();
+        let mut buffer = vec![];
+        match file.read_to_end(&mut buffer) {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("READ FILE ERROR : {} -> {}", e, client_ip);
+                client.write(b"Error reading requested file.");
+                return;
             }
         }
-        Err(e) => eprintln!("READ ERROR {} <- {}", e, client.peer_addr().unwrap().ip()),
+        match client.write_all(&buffer) {
+            Ok(_) => println!("SEND '{}' -> {}", selector, client_ip),
+            Err(e) => eprintln!("SEND FILE ERROR: {} -> {}", e, client_ip),
+        }
+    } else {
+        let mut menu_file = dest.to_path_buf();
+        menu_file.push("index.gph");
+        let menu_file = menu_file.as_path();
+        if menu_file.exists() {
+            let contents = read_file(&menu_file);
+            match contents {
+                Ok(contents) => {
+                    for line in contents.lines() {
+                        let mut line = line.to_owned();
+                        line += "\r\n";
+                        line = line
+                                .replace("$ADDRESS$", ipaddr.as_ref()) // IP replacement
+                                .replace("\\t", "\t"); // More convenient than inserting tabs manually.
+                        client.write(line.as_bytes());
+                    }
+                    client.write(b".");
+                }
+                Err(_) => {
+                    client.write(b"iFailed to read gophermenu for this directory.\r\n.");
+                    client.write(&format!("1Return home\t/\t{}\t70\r\n.", ipaddr).as_bytes());
+                }
+            }
+        } else {
+            client.write(b"iNo content.\r\n");
+            client.write(&format!("1Return home\t/\t{}\t70\r\n.", ipaddr).as_bytes());
+        }
     }
-}
-
-fn format(ltype: &str, display: &str, selector: &str, domain: &str, port: u32) -> String {
-    format!(
-        "{}{}\t{}\t{}\t{}\r\n",
-        ltype,
-        display,
-        selector,
-        domain,
-        port
-    )
 }
 
 fn read_file<P: AsRef<Path>>(path: P) -> Result<String, std::io::Error> {
